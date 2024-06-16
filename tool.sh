87fe1458993fb8529ef174ded127d6383d182f20
michalsiemkacloud <148682572+michalsiemkacloud@users.noreply.github.com>
2024-06-16 16:40:55 +0200

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -4,7 +4,7 @@ while true; do
     cpu_temp=$(</sys/class/thermal/thermal_zone0/temp)
     cpu_temp_celsius=$((cpu_temp / 1000))
     rpm=$(</sys/devices/platform/cooling_fan/hwmon/*/fan1_input)
-    gpu_temp=$(vcgencmd measure_temp | grep -o '[0-9]*\.[0-9]*' | head -n1)
+    gpu_temp=$(vcgencmd measure_temp | grep -o '[0-9]*\.[0-9]*' | head -n1 | awk '{printf "%d", $1}')
     ram_usage=$(free -m | awk '/^Mem:/ {print $3 "/" $2 " MB"}')
     swap_usage=$(free -m | awk '/^Swap:/ {print $3 "/" $2 " MB"}')
     cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1 "%"}')
